a6677f31e8814217c2590be4f8ef634e5920b0a9
vlady <vladysha1021@gmail.com>
2025-03-12 13:56:42 -0400

    Set main.ts in api-gateaway folder

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://rabbitmq:5672'],
+      queue: 'api_gateway_queue',
+      queueOptions: { durable: false },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+  console.log('API Gateway is running on http://localhost:3000');
 }
 bootstrap();
