d95233df9ed9f0acb5546971a1ead3d6b0718d25
vlady <vladysha1021@gmail.com>
2025-03-12 14:05:08 -0400

    Set main.ts in user-service folder

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://rabbitmq:5672'],
+      queue: 'user-service',
+      queueOptions: { durable: false },
+    },
+  });
+
+  await app.listen();
+  console.log('User Service is running and connected to RabbitMQ');
 }
 bootstrap();
